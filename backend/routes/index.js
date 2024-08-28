6044b29589329aa1dd1a1adfeaf6c206917b3efc
Omkara07 <singh.omkara07@gmail.com>
2024-08-29 00:20:35 +0530

    hello route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,8 +4,11 @@ const watchR = require('./watchlist')
 
 const router = express.Router();
 
-router.use("/user",userR)
-router.use("/watchlist",watchR)
+router.use("/user", userR)
+router.use("/watchlist", watchR)
+router.get("/hello", (req, res) => {
+    res.send("Hello There!")
+})
 
 
 module.exports = router
